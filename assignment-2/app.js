67fa3713f006ad5da2a4fab11c0e6b3348a65577
ajr <reanojohn@gmail.com>
2024-12-01 16:54:09 +1000

    Reorder middleware to suit correct execution

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -16,24 +16,25 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(helmet());
-app.use(validateQuery());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(logger("dev")); //
+app.use(cookieParser()); //
+app.use(express.static(path.join(__dirname, "public"))); //
 
 app.use(function (req, res, next) {
   req.db = knex;
   next();
 });
-
 app.use("/", require("./routes/index"));
 app.use("/user", require("./routes/user"));
 app.use("/movies", require("./routes/movies"));
 app.use("/posters", require("./routes/posters"));
+app.use(validateQuery());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
