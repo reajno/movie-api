2b78e930b1ea2611e9de3b8b7f339698a3a3c844
ajr <reanojohn@gmail.com>
2024-12-02 21:23:45 +1000

    Improve error handling function

--- assignment-2/functions/utils/handleError.js
+++ assignment-2/functions/utils/handleError.js
@@ -1,4 +1,8 @@
 module.exports = (res, error) => {
+  console.log(error);
   const statusCode = error.statusCode || 500;
-  res.status(statusCode).json({ error: true, message: error.message });
+  res.status(statusCode).json({
+    error: true,
+    message: error.code ? `Database Error: ${error.code} ${error.sqlMessage}` : error.message,
+  });
 };
